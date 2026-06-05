22eb108bdf4aebdf6f2ecfebc81beb693ff5d866
uazamat877 <usmonovazamat261@gmail.com>
2026-06-05 17:49:19 +0500

    bajarildi

---
+++ task11.py
@@ -0,0 +1,12 @@
+oy = int(input("oy raqamini kiriting:"))
+
+if oy in [12, 1, 2]:
+    print("qish")
+elif oy in [3, 4, 5]:
+    print("bahor")
+elif oy in [6, 7, 8 ]:
+    print("yoz")
+elif oy in [9, 10, 11]:
+    print("kuz")
+else:
+    print("error!")
