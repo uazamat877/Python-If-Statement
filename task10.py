5fa16d9faf723d572af9895f45eabab4a6a01a30
uazamat877 <usmonovazamat261@gmail.com>
2026-06-05 17:40:27 +0500

    bajarildi

---
+++ task10.py
@@ -0,0 +1,12 @@
+bal = int(input("balni kiriting:"))
+
+if 90 < bal < 100:
+    print("A")
+elif 80 < bal <89:
+    print("B")
+elif 70 < bal < 79:
+    print("C")
+elif 60 < bal < 69:
+    print("D")
+else:
+    print("F")
